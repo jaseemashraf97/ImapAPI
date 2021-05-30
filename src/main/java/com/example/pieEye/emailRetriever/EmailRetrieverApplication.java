5e2dce3eb9d8dc31ffd47242cb617a784849b6e2
jaseemashraf97 <jaseemashraf01@gmail.com>
2021-05-31 00:17:03 +0530

    going serverless

--- src/main/java/com/example/pieEye/emailRetriever/EmailRetrieverApplication.java
+++ src/main/java/com/example/pieEye/emailRetriever/EmailRetrieverApplication.java
@@ -2,9 +2,12 @@ package com.example.pieEye.emailRetriever;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class EmailRetrieverApplication {
+//@ComponentScan(basePackages = "com.example.pieEye.emailRetriever.Controller")
+public class EmailRetrieverApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailRetrieverApplication.class, args);
---
+++ src/main/java/com/example/pieEye/emailRetriever/Model/LoginCredentials.java
@@ -0,0 +1,14 @@
+package com.example.pieEye.emailRetriever.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class LoginCredentials {
+    private String email;
+    private  String password;
+
+}
