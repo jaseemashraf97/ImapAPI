2520d1918e7a07f2bc0fff46ffc385576b6ab6d6
jaseemashraf97 <jaseemashraf01@gmail.com>
2021-06-01 14:05:39 +0530

    serverless

--- src/main/java/com/example/pieEye/emailRetriever/EmailRetrieverApplication.java
+++ src/main/java/com/example/pieEye/emailRetriever/EmailRetrieverApplication.java
@@ -2,13 +2,10 @@ package com.example.pieEye.emailRetriever;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-public class EmailRetrieverApplication {
-
+public class EmailRetrieverApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailRetrieverApplication.class, args);
